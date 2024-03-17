5d563cd192ae262af9cfee05a2d8917976f5f019
Labonno Khan Bonna <bonnalabonnokhan2708@gmail.com>
2024-03-17 11:41:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react'
+import {useState} from 'react';
 function App() {
+  const [name, setName]= useState(" ");
+  const [tnc, setTnc] = useState(false);
+  const [interest , setInterest] = useState(" ")
+  function getFormData (e){
+    console.log(name,tnc,interest)
+    e.preventDefault ()
+  }
   return (
+ 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Handle From in React</h1>
+      <form onSubmit={getFormData} >
+        <input type="text" placeholder="enter your name " onChange={(e)=> setName (e.target.value)} /><br /> <br />
+        <select onChange={(e)=>setInterest(e.target.value) }>
+          <option >select option </option>
+          <option >BBA </option>
+          <option >CSE </option>
+          <option >Eng </option>
+        </select> <br /> <br />
+        <input type="checkbox" onChange={(e)=>setTnc(e.target.checked)} /> <span>Accept terms and condition </span> <br /> <br />
+
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
